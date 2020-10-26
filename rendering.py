81a0d500cc51b5468355c1f498dd17e046142dae
Hanlin <hanlinGao@outlook.com>
2020-10-26 14:23:32 -0400

    add origin rendering part

--- rendering.py
+++ rendering.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation
 import pickle
 
 
-def read_data(file):
+def read_predict_data(file):
     points = []
     with open(file, 'rb') as f:
         while True:
@@ -17,21 +17,30 @@ def read_data(file):
     return points
 
 
-points = read_data('predict.pkl')
+def read_origin_data(file):
+    with open(file, 'rb') as f:
+        data = pickle.load(f)
+        box = np.array(data[0][3])    # box remains the same, so just choose the 0th timestep
+        # box_normals = data[0][4]    # box_normals remains the same
+        points = [np.array(x) for x in data[:, 0]]
+        # points = np.array(data[:, 0])     # pos for each timestep
+    return box, points
 
-for each in points:
-    print('particle1 x ,y : ', each[0][0], each[0][1])
-# fig = plt.figure()
+
+box, points = read_origin_data('origin.pkl')
+print('box', box)
+print('points', points)
+
+fig = plt.figure()
 # plt.xlim(-100, 100)
 # plt.ylim(0, 500)
-# point_ani, = plt.plot(points[0][:, 0], points[0][:, 1], "ro")
-# # print(point_ani)
-#
-#
-# def update_points(frame):
-#     point_ani.set_data(points[frame][:, 0], points[frame][:, 1])
-#     return point_ani,
-#
-#
-# ani = FuncAnimation(fig, update_points, frames=len(points), blit=True)
-# plt.show()
+point_ani, = plt.plot(points[0][:, 0], points[0][:, 1], "ro")
+plt.plot(box[:, 0], box[:, 1], "go")
+
+
+def update_points(frame):
+    point_ani.set_data(points[frame][:, 0], points[frame][:, 1])
+    return point_ani,
+
+ani = FuncAnimation(fig, update_points, frames=len(points), blit=True)
+plt.show()

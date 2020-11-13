92df78d03601d9816eb4525bb8e8680d95f1b822
Hanlin <hanlinGao@outlook.com>
2020-11-13 12:29:33 -0500

    update rendering

--- rendering.py
+++ rendering.py
@@ -1,62 +1,156 @@
 import matplotlib; matplotlib.use("TkAgg")
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+import mpl_toolkits.mplot3d.axes3d as p3
 import pickle
 
 
-def read_predict_data(file):
-    points = []
-    with open(file, 'rb') as f:
+# def read_predict_data(file):
+#     points = []
+#     with open(file, 'rb') as f:
+#         while True:
+#             try:
+#                 data = pickle.load(f)
+#                 points.append(data[0].detach().numpy())
+#             except:
+#                 break
+#     return points
+
+
+# def read_origin_data(file):
+#     with open(file, 'rb') as f:
+#         data = pickle.load(f)
+#         box = np.array(data[0][3])    # box remains the same, so just choose the 0th timestep
+#         # box_normals = data[0][4]    # box_normals remains the same
+#         points = [np.array(x) for x in data[:, 0]]
+#         # points = np.array(data[:, 0])     # pos for each timestep
+#     return box, points
+
+
+# def read_numpy_data(file):
+#     points = []
+#     with open(file, 'rb') as f:
+#         while True:
+#             try:
+#                 data = np.load(f)
+#                 points.append(data)
+#             except:
+#                 break
+#     return points
+
+
+# # box, points = read_origin_data('origin.pkl')
+# with open('box_train.pkl', 'rb') as f:
+#     box_data = pickle.load(f)
+#     box = box_data[0]
+# points = read_numpy_data('prediction.npy')
+# print('box', box)
+# print('points', points[0])
+#
+# fig = plt.figure()
+# # plt.xlim(-100, 100)
+# # plt.ylim(0, 500)
+# point_ani, = plt.plot(points[0][:, 0], points[0][:, 1], "ro")
+# plt.plot(box[:, 0], box[:, 1], "go")
+#
+#
+# def update_points(frame):
+#     point_ani.set_data(points[frame][:, 0], points[frame][:, 1])
+#     return point_ani,
+#
+# ani = FuncAnimation(fig, update_points, frames=len(points), blit=True)
+# plt.show()
+
+def rendering_3d(box_file, fluids_file):
+    with open(box_file, 'rb') as f:
+        box_data = pickle.load(f)
+
+    # np.savetxt('box_points.txt', box_data[0])
+    box_x = []
+    box_y = []
+    box_z = []
+    for each in box_data[0]:
+        if each[2] != -0.05:
+            box_x.append(each[0])
+            box_y.append(each[1])
+            box_z.append(each[2])
+    # box_x = box_data[0][:, 0]
+    # box_y = box_data[0][:, 1]
+    # box_z = box_data[0][:, 2]
+
+    # load fluid particles repeatedly since it was saved repeatedly with np.save
+    fluids = []
+    with open(fluids_file, 'rb') as f:
         while True:
             try:
-                data = pickle.load(f)
-                points.append(data[0].detach().numpy())
+                fluids.append(np.load(f))
             except:
                 break
-    return points
 
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+    ax.scatter(box_x, box_z, box_y, s=50, c='gainsboro', alpha=0.4)
+    ax.set_xlim3d(-1, 1, 40)
+    ax.set_zlim3d(0, 4, 80)
+    ax.set_ylim3d(-1, 1, 40)
+    print(len(fluids))
+    def update_graph(frame):
+        data = fluids[frame]
+        graph.set_data(data[:, 0], data[:, 2])
+        graph.set_3d_properties(data[:, 1])
+        return graph,
 
-def read_origin_data(file):
-    with open(file, 'rb') as f:
+    data = np.array(fluids[0])
+    graph, = ax.plot(data[:, 0], data[:, 2], data[:, 1], color='coral', marker='o', fillstyle='full',
+                     markeredgecolor='r', markeredgewidth=0.2, linestyle="")
+    ani = animation.FuncAnimation(fig, update_graph, len(fluids), blit=True)
+
+    plt.show()
+
+def rendering_gt(box_file, fluids_file):
+    with open(box_file, 'rb') as f:
+        box_data = pickle.load(f)
+
+    # np.savetxt('box_points.txt', box_data[0])
+    box_x = []
+    box_y = []
+    box_z = []
+    for each in box_data[0]:
+        if each[2] != -0.05:
+            box_x.append(each[0])
+            box_y.append(each[1])
+            box_z.append(each[2])
+
+    # load fluid particles
+    fluids = []
+    with open(fluids_file, 'rb') as f:
         data = pickle.load(f)
-        box = np.array(data[0][3])    # box remains the same, so just choose the 0th timestep
-        # box_normals = data[0][4]    # box_normals remains the same
-        points = [np.array(x) for x in data[:, 0]]
-        # points = np.array(data[:, 0])     # pos for each timestep
-    return box, points
 
+    for each in data:
+        fluids.append(each[0])
 
-def read_numpy_data(file):
-    points = []
-    with open(file, 'rb') as f:
-        while True:
-            try:
-                data = np.load(f)
-                points.append(data)
-            except:
-                break
-    return points
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+    ax.scatter(box_x, box_z, box_y, s=50, c='gainsboro', alpha=0.4)
+    ax.set_xlim3d(-1, 1, 40)
+    ax.set_zlim3d(0, 4, 80)
+    ax.set_ylim3d(-1, 1, 40)
+    print(len(fluids))
+    def update_graph(frame):
+        data = fluids[frame]
+        graph.set_data(data[:, 0], data[:, 2])
+        graph.set_3d_properties(data[:, 1])
+        return graph,
 
+    data = np.array(fluids[0])
+    graph, = ax.plot(data[:, 0], data[:, 2], data[:, 1], color='coral', marker='o', fillstyle='full',
+                     markeredgecolor='r', markeredgewidth=0.2, linestyle="")
+    ani = animation.FuncAnimation(fig, update_graph, len(fluids), blit=True)
 
-# box, points = read_origin_data('origin.pkl')
-with open('box_train.pkl', 'rb') as f:
-    box_data = pickle.load(f)
-    box = box_data[0]
-points = read_numpy_data('prediction.npy')
-print('box', box)
-print('points', points[0])
+    plt.show()
 
-fig = plt.figure()
-# plt.xlim(-100, 100)
-# plt.ylim(0, 500)
-point_ani, = plt.plot(points[0][:, 0], points[0][:, 1], "ro")
-plt.plot(box[:, 0], box[:, 1], "go")
-
-
-def update_points(frame):
-    point_ani.set_data(points[frame][:, 0], points[frame][:, 1])
-    return point_ani,
-
-ani = FuncAnimation(fig, update_points, frames=len(points), blit=True)
-plt.show()
+# rendering_3d('datasets/box_train.pkl', 'datasets/119_trian.npy')
+rendering_3d('datasets/box_train.pkl', 'datasets/pre_trian.npy')
+# rendering_3d('datasets/box_train.pkl', 'datasets/117_199_trian.npy')
+# rendering_gt('datasets/box_train.pkl', 'datasets/fluid_train.pkl')

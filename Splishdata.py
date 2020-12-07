c55888c2bf214208289794ca9a9927c64f843d80
Hanlin <hanlinGao@outlook.com>
2020-12-06 21:20:31 -0500

    add splishdata process

--- Splishdata.py
+++ Splishdata.py
@@ -5,9 +5,8 @@ import numpy as np
 import pickle
 
 
-def create_raw_data(dir_name):
+def create_raw_data(dir_name, sceneFile):
     output_dir = os.path.join(os.getcwd(), dir_name)
-    sceneFile = 'D:/1_Study/1_ClassMaterials\Master1\Research\splishdata/Sampling_2D.json'
 
     base = sph.Exec.SimulatorBase()
     base.init(sceneFile=sceneFile, outputDir=output_dir)
@@ -23,7 +22,6 @@ def read_vtk(vtk_dir, save_file, mode):
     for root, dirs, files in os.walk(vtk_dir_path):
         files.sort(key=lambda x: int(x[19:-4]))
         for name in files:
-            print(name)
             mesh = pv.read(os.path.join(vtk_dir_path, name))
             pos_matrix = np.array(mesh.points)
             vel_matrix = np.array(mesh.point_arrays['velocity'])
@@ -48,6 +46,8 @@ def read_vtk(vtk_dir, save_file, mode):
     # save into a .pkl file
     with open(save_file, mode) as f:
         pickle.dump(dataset[: -2], f)
+
+    print(len(dataset))
     return
 
 
@@ -80,6 +80,44 @@ def boundary_generation(box_save_file):
         pickle.dump([box_pos, box_normals], f)
 
 
-# create_raw_data('3p_-5_-5')
-read_vtk('3p_-5_-5/vtk', 'fluid_train_3p_-5_-5.pkl', 'wb')
+def dataset_modify(dataset_path, save_path, replicate_times=9):
+    with open(dataset_path, 'rb') as f:
+        dataset = pickle.load(f)
+
+    # check each mapping in the dataset ([pos, vel, label1, label2]), classify them into different phases
+    last_vel = dataset[0][1]    # vel_matrix
+    timesteps_replicated = []
+    num_map = 0
+    for each_map in dataset[1:]:
+        num_map += 1
+        for p_i in range(len(each_map[1])):     # p_i represents particle i
+            if each_map[1][p_i][0] > 0 and last_vel[p_i][0] < 0:
+                print('collision detected', num_map)
+                timesteps_replicated.append(num_map)
+                # print('last vel', last_vel)
+                # print('current vel', each_map[1])
+                break
+        last_vel = each_map[1]
+    print('before modifying', len(dataset))
+    for each in timesteps_replicated:
+        dataset.extend([dataset[each]] * replicate_times)
+    print('after modifying', len(dataset))
+    with open(save_path, 'wb') as f:
+        pickle.dump(dataset, f)
+
+
+# sceneFile = 'D:/1_Study/1_ClassMaterials\Master1\Research\Datasets/Sampling_2D.json'
+# create_raw_data('3p_hori_-2', sceneFile)
+# read_vtk('3p_hori_-2/vtk', '3p_hori_-2.pkl', 'wb')
+
+sceneFile = 'D:/1_Study/1_ClassMaterials\Master1\Research\Datasets/10p.json'
+create_raw_data('20p_hori_0', sceneFile)
+read_vtk('20p_hori_0/vtk', '20p_hori_0.pkl', 'wb')
 # boundary_generation('box_train_box.pkl')
+#
+# create_raw_data('3p_-10_-5')
+# read_vtk('3p_-10_-5/vtk', 'fluid_train_3p_-10_-5.pkl', 'wb')
+
+# create_raw_data('3p_-2_0_vertical')
+# read_vtk('3p_-2_0_vertical/vtk', 'fluid_train_3p_-2_0.pkl', 'wb')
+# dataset_modify('fluid_train_3p_-5_-5.pkl', '3p_-5_-5_modified.pkl')

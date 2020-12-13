1b24bdb9fdae96a4ad6e0205d67efd3526f5186b
Hanlin <hanlinGao@outlook.com>
2020-12-13 15:46:10 -0500

    update Splishdata

--- Splishdata.py
+++ Splishdata.py
@@ -3,6 +3,7 @@ import os
 import pyvista as pv
 import numpy as np
 import pickle
+import json
 
 
 def create_raw_data(dir_name, sceneFile):
@@ -18,11 +19,10 @@ def create_raw_data(dir_name, sceneFile):
 
 def read_vtk(vtk_dir, save_file, mode):
     dataset = []
-    vtk_dir_path = os.path.join(os.getcwd(), vtk_dir)
-    for root, dirs, files in os.walk(vtk_dir_path):
+    for root, dirs, files in os.walk(vtk_dir):
         files.sort(key=lambda x: int(x[19:-4]))
         for name in files:
-            mesh = pv.read(os.path.join(vtk_dir_path, name))
+            mesh = pv.read(os.path.join(vtk_dir, name))
             pos_matrix = np.array(mesh.points)
             vel_matrix = np.array(mesh.point_arrays['velocity'])
             ids = np.array(mesh.point_arrays['id'])
@@ -57,7 +57,7 @@ def boundary_generation(box_save_file):
     box_normals = []
 
     # generate the bottom and ceiling
-    bottom_x = np.arange(-2, 2.05, 0.05)
+    bottom_x = np.concatenate((np.arange(-2, 0, 0.05), np.arange(0, 2.05, 0.05)))
     for each in bottom_x:
         box_pos.append([each, -2., 0.])
         box_normals.append([0., 1., 0.])
@@ -66,7 +66,7 @@ def boundary_generation(box_save_file):
         box_normals.append([0., -1., 0.])
 
     # generate left and right
-    y = np.arange(-1.95, 2., 0.05)
+    y = np.concatenate((np.arange(-1.95, 0, 0.05), np.arange(0, 2, 0.05)))
     for each in y:
         box_pos.append([-2., each, 0.])
         box_normals.append([1., 0., 0.])
@@ -74,50 +74,88 @@ def boundary_generation(box_save_file):
         box_pos.append([2., each, 0.])
         box_normals.append([-1., 0., 0.])
 
+    # generate front and back
+    xv, yv = np.meshgrid(bottom_x, bottom_x)
+    for i in zip(xv.flat, yv.flat):
+        box_pos.append([i[0], i[1], 0.05])
+        box_normals.append([0., 0., -1.])
+
+        box_pos.append([i[0], i[1], -0.05])
+        box_normals.append([0., 0., 1.])
+
     box_pos = np.array(box_pos)
     box_normals = np.array(box_normals)
     with open(box_save_file, 'wb') as f:
         pickle.dump([box_pos, box_normals], f)
 
 
-def dataset_modify(dataset_path, save_path, replicate_times=9):
-    with open(dataset_path, 'rb') as f:
-        dataset = pickle.load(f)
+def create_train_data(jsonfiles, save_file):
+    root_path = os.getcwd()
+    sceneFile = os.path.join(root_path, jsonfiles)
+    train_dir = os.path.join(root_path, save_file)
 
-    # check each mapping in the dataset ([pos, vel, label1, label2]), classify them into different phases
-    last_vel = dataset[0][1]    # vel_matrix
-    timesteps_replicated = []
-    num_map = 0
-    for each_map in dataset[1:]:
-        num_map += 1
-        for p_i in range(len(each_map[1])):     # p_i represents particle i
-            if each_map[1][p_i][0] > 0 and last_vel[p_i][0] < 0:
-                print('collision detected', num_map)
-                timesteps_replicated.append(num_map)
-                # print('last vel', last_vel)
-                # print('current vel', each_map[1])
-                break
-        last_vel = each_map[1]
-    print('before modifying', len(dataset))
-    for each in timesteps_replicated:
-        dataset.extend([dataset[each]] * replicate_times)
-    print('after modifying', len(dataset))
-    with open(save_path, 'wb') as f:
-        pickle.dump(dataset, f)
+    for root, ds, fs in os.walk(sceneFile):
+        for scene in fs:
+            full_name = os.path.join(sceneFile, scene)
+            dir_name = os.path.splitext(scene)[0]
+
+            vtk_save = os.path.join(root_path + '/VTK', dir_name)
+            create_raw_data(vtk_save, full_name)
+            read_vtk(vtk_save + '/vtk', os.path.join(train_dir, dir_name + '.pkl'), 'wb')
 
 
-# sceneFile = 'D:/1_Study/1_ClassMaterials\Master1\Research\Datasets/Sampling_2D.json'
-# create_raw_data('3p_hori_-2', sceneFile)
-# read_vtk('3p_hori_-2/vtk', '3p_hori_-2.pkl', 'wb')
+def create_jsons(configuration_file, save_dir, json_template='D:/1_Study/1_ClassMaterials/Master1/Research/Datasets/Sampling_2D.json'):
+    root_path = 'D:/1_Study/1_ClassMaterials/Master1/Research/Datasets'
+    json_save = os.path.join(root_path, save_dir)
 
-sceneFile = 'D:/1_Study/1_ClassMaterials\Master1\Research\Datasets/10p.json'
-create_raw_data('20p_hori_0', sceneFile)
-read_vtk('20p_hori_0/vtk', '20p_hori_0.pkl', 'wb')
-# boundary_generation('box_train_box.pkl')
-#
-# create_raw_data('3p_-10_-5')
-# read_vtk('3p_-10_-5/vtk', 'fluid_train_3p_-10_-5.pkl', 'wb')
+    with open(json_template, 'r') as f:
+        info = json.load(f)
 
-# create_raw_data('3p_-2_0_vertical')
-# read_vtk('3p_-2_0_vertical/vtk', 'fluid_train_3p_-2_0.pkl', 'wb')
-# dataset_modify('fluid_train_3p_-5_-5.pkl', '3p_-5_-5_modified.pkl')
+    with open(configuration_file, 'r') as f:
+        data_to_generate = json.load(f)
+
+    for each_group in data_to_generate:
+        print('each_group', each_group)
+        count = 0
+        for setting in data_to_generate[each_group]:
+            print('setting', setting)
+            info['FluidBlocks'][0]['start'] = setting['start']
+            info['FluidBlocks'][0]['end'] = setting['end']
+            info['FluidBlocks'][0]['initialVelocity'] = setting['initialVelocity']
+
+            json_name = each_group + '_' + str(count) + '.json'
+            with open(os.path.join(json_save, json_name), 'w') as fw:
+                json.dump(info, fw)
+            count += 1
+
+
+def dataset_integrate(dataset_dir, save_trainset):
+    for root, ds, fs in os.walk(os.path.join(os.getcwd(), dataset_dir)):
+        dataset = []
+        for pkl in fs:
+            with open(os.path.join(root, pkl), 'rb') as f:
+                dataset.extend(pickle.load(f))
+        print(len(dataset))
+
+        # save into a .pkl file
+        with open(os.path.join(root, save_trainset), 'wb') as fw:
+            pickle.dump(dataset, fw)
+
+
+def eval_set(json_file, save_file):
+    root = os.getcwd()
+    vtkfile = os.path.join(root + '/VTK', 'eval')
+    create_raw_data(vtkfile, os.path.join(root, json_file))
+    read_vtk(vtkfile + '/vtk', os.path.join(root, save_file), 'wb')
+
+
+# create_jsons('Jsonfiles/3p')
+# create_train_data('Jsonfiles/3p', '3p_trainset')
+# dataset_integrate('3p_trainset', '3p_trainset.pkl')
+# eval_set('eval.json', 'eval.pkl')
+# create_jsons('data_setting.json', 'Jsonfiles/3p_falling')
+# create_train_data('Jsonfiles/3p_falling', '3p_falling')
+# dataset_integrate('3p_falling', '3p_falling.pkl')
+# eval_set('eval.json', 'eval.pkl')
+
+boundary_generation('box_train_4wall.pkl')
